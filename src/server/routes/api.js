76c8e7d8569c51e8fc24717eb352b54aaa927454
Your Name <you@example.com>
2019-12-17 16:29:51 +0200

    dogs and parks dummy data

---
+++ src/server/routes/api.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const router = express.Router()
+const apiKey = "AIzaSyBJIbKNrO_UfxyAeFsFsJwSqYYKg7_MHRk"
+
+"https://maps.googleapis.com/maps/api/place/textsearch/json?query=dogpark+telaviv+israel&key=AIzaSyBJIbKNrO_UfxyAeFsFsJwSqYYKg7_MHRk"
+module.exports = router

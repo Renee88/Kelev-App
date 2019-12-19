a8fa41d9f8e274c70fcc0a80f036886a13c43bbb
דניאל אלגרסי <Danielelgressy@gmail.com>
2019-12-19 07:38:06 +0200

    fixing the slider

--- src/components/DogsInPark.jsx
+++ src/components/DogsInPark.jsx
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 const dogs = require("../dummyData/dogs.json")
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faPaw, faTimes, faTimesCircle, faCoffee } from '@fortawesome/free-solid-svg-icons'
+// import { library } from '@fortawesome/fontawesome-svg-core'
+
 
 class DogsInPark extends Component {
     
     render() {  
         return (
             <div>
-                <div id="spans">
-                <span id="atThePark">3 at the park</span><span id="onTheWay">2 on the way</span>
-                </div>
+
                 <div id="inPark">
                {dogs.map(d => <div>
                   <img id="pic" src={d.picture}></img> 
-                  <span id="name"><i class="fas fa-circle"></i>{d.dog_name} </span>
-                  <span id="name">{d.dog_name} </span>
+                  <span id="name"><i className="far fa-circle"></i>{d.dog_name} </span>
+                  <span id="timeToArrive">{d.dog_s} </span>
                </div> )}
             </div>
             </div>

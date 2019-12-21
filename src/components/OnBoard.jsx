39a681e73b770380f98c61238f7ddb7c82fa23f9
דניאל אלגרסי <Danielelgressy@gmail.com>
2019-12-21 23:53:14 +0200

    adding touch up

--- src/components/OnBoard.jsx
+++ src/components/OnBoard.jsx
@@ -5,7 +5,14 @@ import "slick-carousel/slick/slick-theme.css";
 import img1 from '../pictures/5.png'
 import img2 from '../pictures/6.png'
 import img3 from '../pictures/7.png'
+import onTheWay from '../pictures/onTheWay.png'
+import FindPark from '../pictures/FindPark.png'
+import AtThePark from '../pictures/AtThePark.png'
+
+
 import '../styles/flickity.css'
+import { Button, Icon } from 'antd';
+
 export default class SimpleSlider extends Component {
     render() {
         const settings = {
@@ -24,18 +31,22 @@ export default class SimpleSlider extends Component {
                 <Slider {...settings}>
                     <div className="test">
                         <div className="illustration">
-                            <img id="img1" src={img1} alt=""/>
+                            <img id="img1" src={AtThePark} alt="" />
                         </div>
                         <h2>SEE WHO IS AT THE PARK</h2>
                         <span className="text">Watch who currently is at the park Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eu velit non</span>
                     </div>
                     <div className="test">
-                        <div className="illustration">2</div>
+                        <div className="illustration">
+                            <img id="img1" src={onTheWay} alt="" />
+                        </div>
                         <h2>SEE WHO IS ON THEIR WAY</h2>
                         <span className="text">Send a message and schedule mutual trips Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eu velit non</span>
                     </div>
                     <div className="test">
-                        <div className="illustration"> 3</div>
+                        <div className="illustration">
+                            <img id="img1" src={FindPark} alt="" />
+                        </div>
                         <h2>FIND THE CLOSEST DOG PARK</h2>
                         <span className="text">Locate a park, watch stats, photos, reviews and what is the best time to come</span>
                     </div>
@@ -43,10 +54,15 @@ export default class SimpleSlider extends Component {
                 </Slider>
 
                 <div className="buttons">
-                    <button type="button" id="forward" > previous</button>
-                    <button type="button" id="previous" > forward</button>
+                    {/* <button type="button" id="forward" > previous</button> */}
+                    {/* <button type="sbutton" id="previous" > forward</button> */}
+                    <Icon id="leftIcon" type="left" />
+                    <Icon id="rightIcon" type="right" />
                 </div>
-
+                <Button id="startBtn" type="primary" size="large">
+                    Start
+                     </Button>
+                <p>Already a member? Login</p>
             </div>
 
         );

3156b749bb2d1f59824c26897320a848b337ebb6
דניאל אלגרסי <Danielelgressy@gmail.com>
2019-12-22 20:03:08 +0200

    adding a input page

--- src/components/Profile/AddDog.jsx
+++ src/components/Profile/AddDog.jsx
@@ -2,13 +2,12 @@ import React, { Component } from 'react';
 // import '../App.css';
 // import '../styles/parkComponent/mainComponent.css';
 
-import { Layout, Divider, Avatar, Icon, Button , InputNumber } from 'antd';
+import { Layout, Divider, Input, Avatar, Icon, Button, InputNumber } from 'antd';
 
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import { observer, inject } from 'mobx-react';
-import '../../styles/profile/Profile.css'
+import '../../styles/profile/AddDog.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Checkbox } from 'antd';
 // import { Avatar, Icon, Button } from 'antd';
 import '../../styles/profile/Profile.css'
 
@@ -21,27 +20,70 @@ const { Header, Footer, Sider, Content } = Layout;
 class AddDog extends Component {
     state = {
         disabled: true,
-      };
-    
-      toggle = () => {
+    };
+
+    toggle = () => {
         this.setState({
-          disabled: !this.state.disabled,
+            disabled: !this.state.disabled,
         });
-      };
-    
-      render() {
+    };
+
+    render() {
         return (
-          <div>
-            <InputNumber min={1} max={10} disabled={this.state.disabled} defaultValue={3} />
-            <div style={{ marginTop: 20 }}>
-              <Button onClick={this.toggle} type="primary">
-                Toggle disabled
-              </Button>
+
+            <div className="dogInputs">
+
+                <div className="dogNameDiv">
+                    <h1>here will be a div for pic</h1>
+                </div>
+
+                <Divider id="divider" />
+
+                <div className="dogNameDiv">
+                    <Input id="inputDogName" size="large" placeholder="Dogs Name" disabled={this.state.disabled} />
+                    <Button id="btnDogName" onClick={this.toggle} type="primary">
+                        Edit
+                    </Button>
+                </div>
+
+                <Divider id="divider" />
+
+                <div className="dogNameDiv">
+                    <Input id="inputDogName" size="large" placeholder="Age" disabled={this.state.disabled} />
+                    <Button id="btnDogName" onClick={this.toggle} type="primary">
+                        Edit
+                    </Button>
+                </div>
+
+                <Divider id="divider" />
+
+                <div className="dogNameDiv">
+                    <Input id="inputDogName" size="large" placeholder="Dogs Name" disabled={this.state.disabled} />
+                    <Button id="btnDogName" onClick={this.toggle} type="primary">
+                        Gender
+                    </Button>
+                </div>
+
+                <Divider id="divider" />
+
+                <div className="dogNameDiv">
+                    <h1>here will be the Id of the dog</h1>
+                </div>
+
+                <Divider id="divider" />
+
+                <div className="dogNameDiv">
+                    {/* <Input id="inputDogName" size="large" placeholder="Dogs Name" disabled={this.state.disabled} /> */}
+                    <Button id="btnDogName" onClick={this.toggle} type="primary">
+                        DELETE
+                    </Button>
+                </div>
+
             </div>
-          </div>
+
         );
-      }
-    
+    }
+
 }
 
 export default AddDog;

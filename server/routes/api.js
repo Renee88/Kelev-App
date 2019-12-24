94e8cbc70c3c04f9c3e59d7ba1c3dcffe75a3ed4
Gili Sinai <gilisinai2016@gmail.com>
2019-12-24 14:21:39 +0200

    add status fanctinality

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,8 +4,8 @@ const apiKey = "AIzaSyBJIbKNrO_UfxyAeFsFsJwSqYYKg7_MHRk"
 const chosenCity = "telaviv"
 const Sequelize = require('sequelize')
 // const sequelize = new Sequelize('mysql://root:@localhost/kelev_app')
-const sequelize = new Sequelize('mysql://root:@localhost/sql_intro')
-// const sequelize = new Sequelize('mysql://root:Gilisinai1@localhost/sql_intro')
+// const sequelize = new Sequelize('mysql://root:@localhost/sql_intro')
+const sequelize = new Sequelize('mysql://root:Gilisinai1@localhost/sql_intro')
 const requestPromise = require('request-promise')
 // const decodePolyline = require('decode-google-map-polyline');
 
